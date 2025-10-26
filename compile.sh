5e7202d6c1fe43aaadfcb01c27369f2018d88dbc
mikhail.karbat <mikhail.karbat@proton.me>
2025-10-26 15:00:57 +0300

    добавлен тестовый скрипт для компиляции прошивки

---
+++ compile.sh
@@ -0,0 +1,7 @@
+# copy all files & directories to ˜/qmk_firmware/keyboards/sofle/keymaps/default
+cp -r * ~/qmk_firmware/keyboards/sofle/keymaps/default
+# compile for rp2040
+qmk compile -kb sofle/rev1 -km default -e CONVERT_TO=rp2040_ce
+# copy firmware here
+cp ~/qmk_firmware/sofle_rev1_default_rp2040_ce.uf2 ./sofle_rev1_default_rp2040_ce.uf2
+
