f1a26e18bde54a6c3b3089fc02a47f0627ffd30e
mikhail.karbat <mikhail.karbat@proton.me>
2025-11-05 15:14:40 +0300

    readme

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
-# copy all files & directories to ˜/qmk_firmware/keyboards/sofle/keymaps/default
-cp -r sofle_v1_lang_shift/* ~/qmk_firmware/keyboards/sofle/keymaps/default
+# copy all files & directories to qmk_firmware
+mkdir -p ~/qmk_firmware/keyboards/sofle/keymaps/sofle_custom_rus
+cp -r sofle_v1_lang_shift/* ~/qmk_firmware/keyboards/sofle/keymaps/sofle_custom_rus
 # compile for rp2040
-qmk compile -kb sofle/rev1 -km default -e CONVERT_TO=rp2040_ce
+qmk compile -kb sofle/rev1 -km sofle_custom_rus -e CONVERT_TO=rp2040_ce
 # copy firmware here
-cp ~/qmk_firmware/sofle_rev1_default_rp2040_ce.uf2 ./sofle_rev1_default_rp2040_ce.uf2
+cp ~/qmk_firmware/sofle_rev1_sofle_custom_rus_rp2040_ce.uf2 ./sofle_rev1_sofle_custom_rus_rp2040_ce.uf2
 
--- flash.sh
+++ flash.sh
@@ -1,4 +1,5 @@
-# copy all files & directories to ˜/qmk_firmware/keyboards/sofle/keymaps/default
-cp -r sofle_v1_lang_shift/* ~/qmk_firmware/keyboards/sofle/keymaps/default
+# copy all files & directories to qmk_firmware
+mkdir -p ~/qmk_firmware/keyboards/sofle/keymaps/sofle_custom_rus
+cp -r sofle_v1_lang_shift/* ~/qmk_firmware/keyboards/sofle/keymaps/sofle_custom_rus
 # compile for rp2040
-qmk flash -kb sofle/rev1 -km default -e CONVERT_TO=rp2040_ce
+qmk flash -kb sofle/rev1 -km sofle_custom_rus -e CONVERT_TO=rp2040_ce
